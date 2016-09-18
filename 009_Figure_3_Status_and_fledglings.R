cfe5c8ea92a671851f4d74295a5eaefd0fadbc3c
ASanchez-Tojar <asanchez@orn.mpg.de>
2016-09-18 12:07:05 +0200

    ylab size

--- 009_Figure_3_Status_and_fledglings.R
+++ 009_Figure_3_Status_and_fledglings.R
@@ -304,7 +304,7 @@ plot(data.plot6$StElo,
      frame.plot = FALSE)
 
 #title(xlab="Standardized Elo-rating", line=4, cex.lab=2.5, family="serif")
-title(ylab="fledglings", line=4.5, cex.lab=2.8, family="serif")
+title(ylab="fledglings", line=4.5, cex.lab=3.2, family="serif")
 
 axis(1,at=seq(0,1,by=0.2),
      las=1,
