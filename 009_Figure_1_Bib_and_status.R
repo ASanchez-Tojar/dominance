6d441723e5723eaecade13071eb640325fadcdfc
ASanchez-Tojar <asanchez@orn.mpg.de>
2016-09-18 12:06:38 +0200

    few changes

--- 009_Figure_1_Bib_and_status.R
+++ 009_Figure_1_Bib_and_status.R
@@ -119,7 +119,7 @@ newdat$upper<-apply(fitmatboth, 1, quantile, prob= 0.975)
 data.plot2 <- final.cap.db.3[!(is.na(final.cap.db.3$age2014.mean)) &
                                !(is.na(final.cap.db.3$TarsusLength)) &
                                !(is.na(final.cap.db.3$meanVB.mean10)) &
-                               #final.cap.db.3$age2014.mean<5&
+                               final.cap.db.3$age2014.mean<5&
                                final.cap.db.3$meanVB.mean10>41,
                              ]
 
@@ -193,11 +193,11 @@ chocolate1 <- c(255,127,36)/rgbing
 # tiff("plots/Figure1_Status_and_Bib.tiff", height=20, width=20,
 #      units='cm', compression="lzw", res=300)
 
-tiff("plots/talks/Figure1_Status_and_Bib_talk.tiff", height=20, width=20,
-     units='cm', compression="lzw", res=300)
-
-# tiff("plots/talks/Figure1_Status_and_Bib_talk_noelderly.tiff", height=20, width=20,
+# tiff("plots/talks/Figure1_Status_and_Bib_talk.tiff", height=20, width=20,
 #      units='cm', compression="lzw", res=300)
+
+tiff("plots/talks/Figure1_Status_and_Bib_talk_noelderly.tiff", height=20, width=20,
+     units='cm', compression="lzw", res=300)
 
 #par(mar=c(5, 5, 1, 1))
 par(mar=c(6, 7, 1, 1))
@@ -216,8 +216,8 @@ plot(data.plot1$bib,
      family="serif",
      frame.plot = FALSE)
 
-title(xlab="Bib length (mm)", line=4, cex.lab=2.5, family="serif")
-title(ylab="Standardized Elo-rating", line=5, cex.lab=2.5, family="serif")
+title(xlab="Bib length (mm)", line=4, cex.lab=3.0, family="serif")
+title(ylab="Standardized Elo-rating", line=4.5, cex.lab=3.0, family="serif")
 
 axis(1,at=seq(43,59,by=2),
      #cex.axis=1.3,
