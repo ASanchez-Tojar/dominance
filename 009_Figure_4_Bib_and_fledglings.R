a37d96b0ca75fe065aac060526ac1bd348328798
ASanchez-Tojar <asanchez@orn.mpg.de>
2016-09-18 11:23:10 +0200

    adding changes to get a talk plot

--- 009_Figure_4_Bib_and_fledglings.R
+++ 009_Figure_4_Bib_and_fledglings.R
@@ -334,29 +334,40 @@ blue <- c(44,127,184)/rgbing
 
 # PLOT saved as .tiff
 
-tiff("plots/Figure4_Bib_and_geneticfledglings.tiff", height=20, width=20,
+# tiff("plots/Figure4_Bib_and_geneticfledglings.tiff", height=20, width=20,
+#      units='cm', compression="lzw", res=300)
+
+tiff("plots/talks/Figure4_Bib_and_geneticfledglings_talk.tiff", height=20, width=20,
      units='cm', compression="lzw", res=300)
 
-par(mar=c(5, 5, 1, 1))
+#par(mar=c(5, 5, 1, 1))
+par(mar=c(6, 7, 1, 1))
 
 plot(data.plot7$bib, 
      data.plot7$gen.fledg.12d, 
      type="n",
-     xlab="Bib length (mm)",
-     ylab= "Annual number of fledglings",
+#      xlab="Bib length (mm)",
+#      ylab= "Annual number of fledglings",
+     xlab="",
+     ylab="",
      cex.lab=1.7,
      xaxt="n",yaxt="n",xlim=c(44,58),ylim=c(0,14),
      family="serif",
      frame.plot = FALSE)
 
+title(xlab="Bib length (mm)", line=4, cex.lab=2.5, family="serif")
+title(ylab="Annual number of fledglings", line=5, cex.lab=2.5, family="serif")
+
 
 axis(1,at=seq(44,58,by=2),
      las=1,
-     cex.axis=1.3,
+     #cex.axis=1.3,
+     cex.axis=1.8,
      family="serif") 
 
 axis(2,at=seq(0,14,by=1),
-     cex.axis=1.3,
+     #cex.axis=1.3,
+     cex.axis=1.8,
      las=2,
      family="serif")
 
