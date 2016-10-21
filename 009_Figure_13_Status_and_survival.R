5b535459edee9f4e677c4476908083be6a67e51e
ASanchez-Tojar <asanchez@orn.mpg.de>
2016-10-21 17:34:43 +0200

    doing plots with simulated StElos

--- 009_Figure_13_Status_and_survival.R
+++ 009_Figure_13_Status_and_survival.R
@@ -31,22 +31,42 @@ rm(list=ls())
 survival <- read.table("survival/forsurv_jj.csv",header=TRUE,sep=",")
 
 
+# Repeating with simulated StElo
 
-# Subsetting the database to only those that interacted more than 8 times per event
+# survival$BirdID_eventSW <- factor(paste(survival$BirdID,
+#                                      survival$eventSW,sep="_"))
+# 
+# 
+# # using simulated StElo valuse
+# 
+# sim_StElo <- read.table("finaldatabases/rank.TLandM.VB.fitness_sim.csv",header=TRUE,sep=",")
+# 
+# sim_StElo.2 <- sim_StElo[,c("BirdID_eventSW","StElo")]
+# 
+# 
+# # droping old StElo and adding the new simulated one
+# 
+# dropoldStElo <- c("StElo")
+# survival <- survival[ , !(names(survival) %in% dropoldStElo)]
+# 
+# survival <- merge(survival,sim_StElo.2,by="BirdID_eventSW")
 
-morethan8pereventSW <- read.table("morethan8pereventSW.csv",header=TRUE,sep=",")
 
-intpereventSW <- morethan8pereventSW[,c("indevent","freqppereventSW")]
-
-names(intpereventSW)<-c("ind_eventSW","freqppereventSW")
-
-survival$ind_eventSW <- factor(paste(survival$individual,
-                                     survival$eventSW,sep="_"))
-
-survival.9int <- merge(survival,intpereventSW,
-                       by="ind_eventSW",all.y=TRUE)
-
-survival <- survival.9int[!(is.na(survival.9int$BirdID)),]
+# # Subsetting the database to only those that interacted more than 8 times per event
+# 
+# morethan8pereventSW <- read.table("morethan8pereventSW.csv",header=TRUE,sep=",")
+# 
+# intpereventSW <- morethan8pereventSW[,c("indevent","freqppereventSW")]
+# 
+# names(intpereventSW)<-c("ind_eventSW","freqppereventSW")
+# 
+# survival$ind_eventSW <- factor(paste(survival$individual,
+#                                      survival$eventSW,sep="_"))
+# 
+# survival.9int <- merge(survival,intpereventSW,
+#                        by="ind_eventSW",all.y=TRUE)
+# 
+# survival <- survival.9int[!(is.na(survival.9int$BirdID)),]
 
 
 ################################################################
@@ -121,11 +141,18 @@ survival.f <- survival[survival$SexEstimate==0,]
 
 # PLOT saved as .tiff
 
-# tiff("plots/talks/Figure13_Status_and_Survival.tiff", height=20, width=20,
+tiff("plots/talks/Figure13_Status_and_Survival.tiff", height=20, width=20,
+     units='cm', compression="lzw", res=300)
+
+# tiff("plots/talks/9interactions/Figure13_Status_and_Survival_9int.tiff", height=20, width=20,
 #      units='cm', compression="lzw", res=300)
 
-tiff("plots/talks/9interactions/Figure13_Status_and_Survival_9int.tiff", height=20, width=20,
-     units='cm', compression="lzw", res=300)
+# tiff("plots/talks/Figure13_Status_and_Survival_sim.tiff", height=20, width=20,
+#      units='cm', compression="lzw", res=300)
+
+# tiff("plots/talks/9interactions/Figure13_Status_and_Survival_9int_sim.tiff", height=20, width=20,
+#      units='cm', compression="lzw", res=300)
+
 
 #par(mar=c(5, 5, 1, 1))
 par(mar=c(6, 7, 1, 1))
