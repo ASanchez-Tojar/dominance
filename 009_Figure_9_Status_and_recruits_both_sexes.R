d80c3a13dfd6de498b60b5f17796888b4ee555b2
ASanchez-Tojar <asanchez@orn.mpg.de>
2017-02-14 22:55:48 +0100

    updating plots for silwood talk

--- 009_Figure_9_Status_and_recruits_both_sexes.R
+++ 009_Figure_9_Status_and_recruits_both_sexes.R
@@ -29,8 +29,8 @@ rm(list=ls())
 
 # loading the clean databases from Lundy with all the data needed
 
-#rank.TLandM.VB.fitness <- read.table("finaldatabases/rank.TLandM.VB.fitness.csv",header=TRUE,sep=",")
-rank.TLandM.VB.fitness <- read.table("finaldatabases/rank.TLandM.VB.fitness.9int.csv",header=TRUE,sep=",")
+rank.TLandM.VB.fitness <- read.table("finaldatabases/rank.TLandM.VB.fitness.csv",header=TRUE,sep=",")
+#rank.TLandM.VB.fitness <- read.table("finaldatabases/rank.TLandM.VB.fitness.9int.csv",header=TRUE,sep=",")
 #rank.TLandM.VB.fitness <- read.table("finaldatabases/rank.TLandM.VB.fitness_sim.csv",header=TRUE,sep=",")
 #rank.TLandM.VB.fitness <- read.table("finaldatabases/rank.TLandM.VB.fitness.9int_sim.csv",header=TRUE,sep=",")
 
@@ -206,11 +206,11 @@ chocolate1 <- c(255,127,36)/rgbing
 # tiff("plots/talks/9interactions/Figure9_Status_and_recruits_both_sexes_2014_9int.tiff", height=20, width=20,
 #      units='cm', compression="lzw", res=300)
 
-# tiff("plots/talks/Figure9_Status_and_recruits_both_sexes_2015_sim.tiff", height=20, width=20,
-#      units='cm', compression="lzw", res=300)
-
-tiff("plots/talks/9interactions/Figure9_Status_and_recruits_both_sexes_2015_9int_sim.tiff", height=20, width=20,
+tiff("plots/talks/Figure9_Status_and_recruits_both_sexes_2014_sim_upd.tiff", height=20, width=20,
      units='cm', compression="lzw", res=300)
+
+# tiff("plots/talks/9interactions/Figure9_Status_and_recruits_both_sexes_2015_9int_sim.tiff", height=20, width=20,
+#      units='cm', compression="lzw", res=300)
 
 
 #par(mar=c(5, 5, 1, 1))
@@ -255,7 +255,7 @@ points(rank.TLandM.VB.fitness.f.2$StElo,
        pch = 19, col=rgb(chocolate1[1],chocolate1[2],chocolate1[3],0.4),       
        cex = 2.0)
 
-index.1<-newdat.m$eventSW=="2015" # only calls the plot but not the points yet
+index.1<-newdat.m$eventSW=="2014" # only calls the plot but not the points yet
 
 polygon(c(newdat.m$StElo[index.1],rev(newdat.m$StElo[index.1])),
         c(newdat.m$lower[index.1],rev(newdat.m$upper[index.1])),
